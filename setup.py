474b9cd96f724869fcb048814e96a3299d3b997d
Vasili Syrakis <cetanu@gmail.com>
2019-01-17 15:03:09 +1100

    0.1.11

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def readme():
 
 setup(
     name='sovereign',
-    version='0.1.10',
+    version='0.1.11',
     python_requires='>=3.6.0',
     packages=find_packages('src'),
     package_dir={'': 'src'},
