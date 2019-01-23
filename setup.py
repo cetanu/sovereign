c34af96b5b63f4d2169a1f6eae25c34a07178173
Vasili Syrakis <cetanu@gmail.com>
2019-01-23 11:21:46 +1100

    0.1.13 (Makes new docs available on dev deployments)

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def readme():
 
 setup(
     name='sovereign',
-    version='0.1.12',
+    version='0.1.13',
     python_requires='>=3.7.0',
     packages=find_packages('src'),
     package_dir={'': 'src'},
