c95c9e46bf4a2778560f49704f751bb6ee152727
Vasili Syrakis <cetanu@gmail.com>
2019-02-02 11:48:16 +1100

    Restructure init so it doesn't die without config (so it can be imported)

--- src/sovereign/__init__.py
+++ src/sovereign/__init__.py
@@ -4,42 +4,41 @@ from datadog import statsd
 from sovereign import config_loader
 from sovereign.logs import LOG
 
+XDS_TEMPLATES = defaultdict(dict)
+TEMPLATE_CONTEXT = dict()
+DEBUG = bool(os.getenv('SOVEREIGN_DEBUG'))
+ENVIRONMENT = os.getenv('SOVEREIGN_ENVIRONMENT_TYPE', os.getenv('MICROS_ENVTYPE', 'local'))
+CONFIG = dict()
+
 try:
     CONFIG_PATHS = os.getenv('SOVEREIGN_CONFIG').split(',')
 except AttributeError:
-    raise RuntimeError('No configuration specified via environment variable SOVEREIGN_CONFIG')
+    LOG.error('No configuration specified via environment variable SOVEREIGN_CONFIG')
+else:
+    for path in CONFIG_PATHS:
+        cfg = config_loader.load(path)
+        CONFIG.update(cfg)
 
-CONFIG = dict()
-for path in CONFIG_PATHS:
-    cfg = config_loader.load(path)
-    CONFIG.update(cfg)
+    _templates = CONFIG['templates'].items()
+    _template_context = CONFIG.get('template_context', {}).items()
 
-_templates = CONFIG['templates'].items()
-_template_context = CONFIG.get('template_context', {}).items()
+    for version, templates in _templates:
+        for _type, path in templates.items():
+            XDS_TEMPLATES[version][_type] = config_loader.load(path)
 
-XDS_TEMPLATES = defaultdict(dict)
-for version, templates in _templates:
-    for _type, path in templates.items():
-        XDS_TEMPLATES[version][_type] = config_loader.load(path)
+    for key, value in _template_context:
+        TEMPLATE_CONTEXT[key] = config_loader.load(value)
 
-TEMPLATE_CONTEXT = dict()
-for key, value in _template_context:
-    TEMPLATE_CONTEXT[key] = config_loader.load(value)
+    if CONFIG.get('statsd', {}).get('enabled'):
+        statsd.host = CONFIG['statsd']['host']
+        statsd.namespace = CONFIG['statsd'].get('namespace', 'sovereign')
 
-if CONFIG.get('statsd', {}).get('enabled'):
-    statsd.host = CONFIG['statsd']['host']
-    statsd.namespace = CONFIG['statsd'].get('namespace', 'sovereign')
-
-DEBUG = bool(os.getenv('SOVEREIGN_DEBUG'))
-
-ENVIRONMENT = os.getenv('SOVEREIGN_ENVIRONMENT_TYPE', os.getenv('MICROS_ENVTYPE', 'local'))
-
-LOG.msg(
-    event='startup',
-    envtype=os.getenv('MICROS_ENVTYPE', os.getenv('SOVEREIGN_ENVIRONMENT_TYPE')),
-    env=ENVIRONMENT,
-    config=CONFIG_PATHS,
-    context=_template_context,
-    templates=_templates,
-    is_debug=DEBUG
-)
+    LOG.msg(
+        event='startup',
+        envtype=os.getenv('MICROS_ENVTYPE', os.getenv('SOVEREIGN_ENVIRONMENT_TYPE')),
+        env=ENVIRONMENT,
+        config=CONFIG_PATHS,
+        context=_template_context,
+        templates=_templates,
+        is_debug=DEBUG
+    )
