37ad206824d5c2dd0da357c095b73e01da1c4237
Vasili Syrakis <vsyrakis@atlassian.com>
2019-10-01 16:54:44 +1000

    Add timeouts

--- test/unit/test_discovery.py
+++ test/unit/test_discovery.py
@@ -2,12 +2,14 @@ import pytest
 from sovereign import discovery
 
 
+@pytest.mark.timeout(0.05)
 @pytest.mark.asyncio
 async def test_discovery(discovery_request, sources):
     config = await discovery.response(discovery_request, 'clusters')
     assert 'httpbin' in repr(config) and 'google-proxy' not in repr(config)
 
 
+@pytest.mark.timeout(0.5)
 @pytest.mark.asyncio
 async def test_discovery_1000(discovery_request, sources_1000):
     config = await discovery.response(discovery_request, 'clusters')
@@ -15,6 +17,7 @@ async def test_discovery_1000(discovery_request, sources_1000):
     assert len(config['resources']) == 1000
 
 
+@pytest.mark.timeout(5)
 @pytest.mark.asyncio
 async def test_discovery_10000(discovery_request, sources_10000):
     config = await discovery.response(discovery_request, 'clusters')
