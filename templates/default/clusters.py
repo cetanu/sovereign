aaaf85160423cc7b46b23a5aa24b5cb6348d41b8
Vasili Syrakis <vsyrakis@atlassian.com>
2020-02-18 14:18:30 +1100

    0.6.13

--- templates/default/clusters.py
+++ templates/default/clusters.py
@@ -4,7 +4,10 @@ def call(instances, discovery_request, **kwargs):
             '@type': 'type.googleapis.com/envoy.api.v2.Cluster',
             'name': instance['name'],
             'connect_timeout': '5s',
-            'tls_context': {},
+            'transport_socket': {
+                'name': 'envoy.transport_sockets.tls',
+                'typed_config': {'@type': 'type.googleapis.com/envoy.api.v2.auth.UpstreamTlsContext'}
+            },
             'type': 'strict_dns',
             'load_assignment': {
                 'cluster_name': f'{ instance["name"] }_cluster',
