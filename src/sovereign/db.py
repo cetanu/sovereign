db630d3c5174335377f27857f5a12e70ada7495c
Vasili Syrakis <vsyrakis@atlassian.com>
2021-05-19 23:51:35 +1000

    in-memory db will have a maximum limit of entries

--- src/sovereign/db.py
+++ src/sovereign/db.py
@@ -1,32 +1,58 @@
 from typing import Optional
 
-try:
-    import redis
-    from simplekv.memory.redisstore import RedisStore
 
-    r = redis.StrictRedis(host="redis", port=6379, db=0)
-    store = RedisStore(r)
-except ImportError:
-    from simplekv.memory import DictStore
+class DictWithCapacity(dict):
+    max_items = 200
 
-    store = DictStore()
+    def __init__(self, *args, **kwargs):
+        self.max_items = kwargs.pop("max_items", self.max_items)
+        super(DictWithCapacity, self).__init__(*args, **kwargs)
+
+    def __setitem__(self, key, val):
+        if key not in self:
+            max_items = self.max_items - 1
+            self._prune(max_items)
+        super(DictWithCapacity, self).__setitem__(key, val)
+
+    def update(self, **kwargs):
+        super(DictWithCapacity, self).update(**kwargs)
+        self._prune(self.max_items)
+
+    def _prune(self, max_items):
+        if len(self) >= max_items:
+            diff = len(self) - max_items
+            for k in list(self.keys())[:diff]:
+                del self[k]
+
+
+def init_store():
+    try:
+        import redis
+        from simplekv.memory.redisstore import RedisStore
+
+        r = redis.StrictRedis(host="redis", port=6379, db=0)
+        return RedisStore(r)
+    except ImportError:
+        from simplekv.memory import DictStore
+
+        return DictStore(d=DictWithCapacity())
 
 
 def put_resources(node_id: str, resource: str, data: bytes, version: str) -> None:
     data_key = f"{node_id}{resource}"
     version_key = f"{node_id}{resource}version"
-    store.put(data_key, data)
-    store.put(version_key, version.encode())
+    STORE.put(data_key, data)
+    STORE.put(version_key, version.encode())
 
 
 def get_resources(node_id: str, resource: str, version: str) -> Optional[bytes]:
     data_key = f"{node_id}{resource}"
     version_key = f"{node_id}{resource}version"
     try:
-        stored_version = store.get(version_key)
+        stored_version = STORE.get(version_key)
         if stored_version == version:
             return None
-        return store.get(data_key)
+        return STORE.get(data_key)
     except KeyError:
         return None
 
@@ -34,6 +60,9 @@ def get_resources(node_id: str, resource: str, version: str) -> Optional[bytes]:
 def version_is_latest(node_id: str, resource: str, version: str) -> bool:
     version_key = f"{node_id}{resource}version"
     try:
-        return store.get(version_key) == version
+        return STORE.get(version_key) == version
     except KeyError:
         return False
+
+
+STORE = init_store()
