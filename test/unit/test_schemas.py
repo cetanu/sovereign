94168a901bbb2acf321005186db0ddd826b43828
Dzuy Pham <dpham2@atlassian.com>
2022-06-21 17:39:52 +1000

    EDGE-4983: Add unit tests for ContextConfiguration schema

---
+++ test/unit/test_schemas.py
@@ -0,0 +1,47 @@
+import pytest
+from sovereign.schemas import ContextConfiguration
+from pydantic import ValidationError
+
+
+@pytest.mark.parametrize("input_rate", [1, 5000, 10000])
+def test_context_configuration_refresh_rate(input_rate: str):
+    context_configuration = ContextConfiguration(
+        context={}, refresh=True, refresh_rate=input_rate
+    )
+
+    assert context_configuration.refresh_rate == input_rate
+
+
+@pytest.mark.parametrize(
+    "input_cron_expression,expect_success",
+    [
+        ("* * * * *", True),
+        ("1 * 2 * 3", True),
+        ("*/5 * * * *", True),
+        ("* * */5 * * * *", False),
+        ("test", False),
+    ],
+)
+def test_context_configuration_refresh_cron(input_cron_expression, expect_success):
+    if expect_success:
+        context_configuration = ContextConfiguration(
+            context={}, refresh=True, refresh_cron=input_cron_expression
+        )
+        assert context_configuration.refresh_cron == input_cron_expression
+    else:
+        with pytest.raises(ValidationError):
+            ContextConfiguration(
+                context={}, refresh=True, refresh_cron=input_cron_expression
+            )
+
+
+def test_context_configuration_sets_default():
+    context_configuration = ContextConfiguration(context={}, refresh=True)
+    assert context_configuration.refresh_rate is not None
+
+
+def test_context_configuration_raises_multiple_refresh_methods():
+    with pytest.raises(RuntimeError):
+        ContextConfiguration(
+            context={}, refresh=True, refresh_rate=5, refresh_cron="* * * * *"
+        )
