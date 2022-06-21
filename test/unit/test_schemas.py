83ecfc8911c1d7307e06972e8a81b515276212ac
Dzuy Pham <dpham2@atlassian.com>
2022-06-21 17:51:30 +1000

    EDGE-4983: Split refresh_cron validity tests

--- test/unit/test_schemas.py
+++ test/unit/test_schemas.py
@@ -4,7 +4,7 @@ from pydantic import ValidationError
 
 
 @pytest.mark.parametrize("input_rate", [1, 5000, 10000])
-def test_context_configuration_refresh_rate(input_rate: int) -> None:
+def test_context_configuration_refresh_rate_valid(input_rate: int) -> None:
     context_configuration = ContextConfiguration(
         context={}, refresh=True, refresh_rate=input_rate
     )
@@ -13,28 +13,35 @@ def test_context_configuration_refresh_rate(input_rate: int) -> None:
 
 
 @pytest.mark.parametrize(
-    "input_cron_expression,expect_success",
+    "input_cron_expression",
     [
-        ("* * * * *", True),
-        ("1 * 2 * 3", True),
-        ("*/5 * * * *", True),
-        ("* * */5 * * * *", False),
-        ("test", False),
+        "* * * * *",
+        "1 * 2 * 3",
+        "*/5 * * * *",
     ],
 )
-def test_context_configuration_refresh_cron(
-    input_cron_expression: str, expect_success: bool
+def test_context_configuration_refresh_cron_valid(input_cron_expression: str) -> None:
+    context_configuration = ContextConfiguration(
+        context={}, refresh=True, refresh_cron=input_cron_expression
+    )
+    assert context_configuration.refresh_cron == input_cron_expression
+
+
+@pytest.mark.parametrize(
+    "input_cron_expression",
+    [
+        "* * */5 * * * *",
+        "0 0 0 */10 *",
+        "test",
+    ],
+)
+def test_context_configuration_refresh_cron_raises_on_invalid_expression(
+    input_cron_expression: str,
 ) -> None:
-    if expect_success:
-        context_configuration = ContextConfiguration(
+    with pytest.raises(ValidationError):
+        ContextConfiguration(
             context={}, refresh=True, refresh_cron=input_cron_expression
         )
-        assert context_configuration.refresh_cron == input_cron_expression
-    else:
-        with pytest.raises(ValidationError):
-            ContextConfiguration(
-                context={}, refresh=True, refresh_cron=input_cron_expression
-            )
 
 
 def test_context_configuration_sets_default() -> None:
