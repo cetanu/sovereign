fb30777ddea3e87282a45ce0be68eb3101ff46e8
Benjamin McAlary <bmcalary@atlassian.com>
2023-04-02 16:05:19 +1000

    more fixes?

--- templates/default/clusters.py
+++ templates/default/clusters.py
@@ -1,12 +1,17 @@
 def call(instances, discovery_request, **kwargs):
-    eds = kwargs['eds']
+    eds = kwargs["eds"]
 
     for instance in instances:
         yield {
             "name": instance["name"],
             "type": "STRICT_DNS",
             "connect_timeout": "5.000s",
-            "tls_context": {},
+            "transport_socket": {
+                "name": "envoy.transport_sockets.tls",
+                "typed_config": {
+                    "@type": "type.googleapis.com/envoy.extensions.transport_sockets.tls.v3.UpstreamTlsContext",
+                },
+            },
             "load_assignment": {
                 "cluster_name": f"{instance['name']}_cluster",
                 "endpoints": eds.locality_lb_endpoints(
