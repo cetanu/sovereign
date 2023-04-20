99d6056d3427e21e9119395ceaac169b43a8d5d3
Tony Tang <tsukiy0@users.noreply.github.com>
2023-04-20 17:50:39 +1000

    test: ensure success metric emitted when successfully loading context
    Signed-off-by: Tony Tang <tsukiy0@users.noreply.github.com>

--- test/unit/test_context.py
+++ test/unit/test_context.py
@@ -2,9 +2,8 @@ from sovereign.context import TemplateContext
 from unittest.mock import Mock
 from sovereign.config_loader import Loadable, Serialization, Protocol
 
-def test_initialises_context() -> None:
+def test_init_context() -> None:
     mock_stats = Mock()
-    mock_stats.increment = Mock()
     configured_context = {
         "foo": Loadable(
             protocol=Protocol.inline,
@@ -25,3 +24,29 @@ def test_initialises_context() -> None:
     )
     
     assert context.context["foo"] == "bar"
+
+def test_emit_metric_when_successfully_init_context() -> None:
+    mock_stats = Mock()
+    mock_stats.increment = Mock()
+    configured_context = {
+        "foo": Loadable(
+            protocol=Protocol.inline,
+            serialization=Serialization.raw,
+            path="bar",
+        ),
+    }
+
+    TemplateContext(
+        refresh_rate=None,
+        refresh_cron=None,
+        configured_context=configured_context,
+        poller=None,
+        encryption_suite=None,
+        disabled_suite=None,
+        logger=None,
+        stats=mock_stats,
+    )
+
+    assert mock_stats.increment.call_count == 1
+    mock_stats.increment.assert_called_with("context.refresh.success", tags=["context:foo"])
+    
