a9b9b3d69793c3aa775c6be7b7affa9ceedcbe51
Tony Tang <tsukiy0@users.noreply.github.com>
2023-04-20 17:54:43 +1000

    feat: ensure emit metric when failed to init context
    Signed-off-by: Tony Tang <tsukiy0@users.noreply.github.com>

--- test/unit/test_context.py
+++ test/unit/test_context.py
@@ -3,7 +3,6 @@ from unittest.mock import Mock
 from sovereign.config_loader import Loadable, Serialization, Protocol
 
 def test_init_context() -> None:
-    mock_stats = Mock()
     configured_context = {
         "foo": Loadable(
             protocol=Protocol.inline,
@@ -20,7 +19,7 @@ def test_init_context() -> None:
         encryption_suite=None,
         disabled_suite=None,
         logger=None,
-        stats=mock_stats,
+        stats=Mock(),
     )
     
     assert context.context["foo"] == "bar"
@@ -49,4 +48,29 @@ def test_emit_metric_when_successfully_init_context() -> None:
 
     assert mock_stats.increment.call_count == 1
     mock_stats.increment.assert_called_with("context.refresh.success", tags=["context:foo"])
+    
+def test_emit_metric_when_failed_to_init_context() -> None:
+    mock_stats = Mock()
+    mock_stats.increment = Mock()
+    configured_context = {
+        "foo": Loadable(
+            protocol=Protocol.file,
+            serialization=Serialization.json,
+            path="bad_path",
+        ),
+    }
+
+    TemplateContext(
+        refresh_rate=None,
+        refresh_cron=None,
+        configured_context=configured_context,
+        poller=None,
+        encryption_suite=None,
+        disabled_suite=None,
+        logger=Mock(),
+        stats=mock_stats,
+    )
+
+    assert mock_stats.increment.call_count == 1
+    mock_stats.increment.assert_called_with("context.refresh.error", tags=["context:foo"])
     
