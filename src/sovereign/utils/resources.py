92ff9a4cd9c08d34c5016b3984eb8ac4afff3dde
Jacob Neil Taylor <jacobneiltaylor@canva.com>
2023-09-20 14:10:38 +1000

    Add type hints

--- src/sovereign/utils/resources.py
+++ src/sovereign/utils/resources.py
@@ -1,16 +1,17 @@
 from functools import cache
 import importlib.resources as res
+from importlib.resources.abc import Traversable
 
 
 @cache
-def get_package(name: str):
+def get_package(name: str) -> Traversable:
     return res.files(name)
 
 
-def get_package_file(package_name: str, filename: str):
+def get_package_file(package_name: str, filename: str) -> Traversable:
     return get_package(package_name).joinpath(filename)
 
 
-def get_package_file_bytes(package_name: str, filename: str):
+def get_package_file_bytes(package_name: str, filename: str) -> bytes:
     file = get_package_file(package_name, filename)
     return file.read_bytes()
