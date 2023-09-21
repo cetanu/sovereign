2fd56afcfeaf597c804e49f88392d5500be0f09c
Vasilios Syrakis <vsyrakis@atlassian.com>
2023-09-21 21:50:54 +1000

    Initial

--- src/sovereign/runtime.py
+++ src/sovereign/runtime.py
@@ -1,8 +1,17 @@
 import json
 from typing import Any
+
+from cachelib import SimpleCache
+
 from sovereign.constants import TEMPLATE_CTX_PATH
 
+_cache = SimpleCache(threshold=5, default_timeout=30)
 
 def template_context() -> Any:
+    ctx = _cache.get("template_context")
+    if ctx is not None:
+        return ctx
     with open(TEMPLATE_CTX_PATH) as f:
-        return json.loads(f.read())
+        ret = json.loads(f.read())
+        _cache.set("template_context", ret)
+        return ret
