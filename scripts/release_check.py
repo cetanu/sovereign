ffe7f1358e4d7cbf08b76c3a5bbd8e63cea61536
Vasilios Syrakis <vsyrakis@atlassian.com>
2024-08-26 11:40:03 +1000

    Add logging release check script

--- scripts/release_check.py
+++ scripts/release_check.py
@@ -1,19 +1,37 @@
+import re
 import sys
 import toml
 
 
+VERSION = re.compile(
+    r"(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)(?P<prerelease>a|b|rc)?(?P<number>\d+)?"
+)
+
+
 def is_pre_release():
     with open("pyproject.toml") as f:
+        print("Reading pyproject.toml")
         project = toml.load(f)
 
     version = project["tool"]["poetry"]["version"]
-    _, _, patch = version.split('.')
+    match_ = VERSION.search(version)
+    if match_:
+        v = match_.groupdict()
+        print(v)
+        prerelease = v.get("prerelease")
 
-    prerelease = any((
-        patch[0] in ["a", "b"],
-        patch[0:1] == "rc"
-    ))
-    return 0 if prerelease else 1
+        if prerelease == "a":
+            print("Alpha release")
+            return 0
+        if prerelease == "b":
+            print("Beta release")
+            return 0
+        if prerelease == "rc":
+            print("Release candidate")
+            return 0
+    return 1
 
 
-sys.exit(is_pre_release())
+result = is_pre_release()
+print(result)
+sys.exit(result)
