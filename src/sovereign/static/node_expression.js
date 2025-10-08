c7e99706742b1783892202960641f531bb4d461a
Vasilios Syrakis <vsyrakis@atlassian.com>
2025-10-08 19:16:37 +1100

    fix[ui]: node expression causes permanent error on invalid input
    the ui will now clear the cookie if it contains invalid data
    and then display an error message until next refresh
    
    node filter input box changes colour for valid/invalid expressions

--- src/sovereign/static/node_expression.js
+++ src/sovereign/static/node_expression.js
@@ -1,10 +1,33 @@
 const input = document.getElementById('filterInput');
+const inputMessage = document.getElementById('filterMessage');
 const form = document.getElementById('filterForm');
+
+function validateInput(inputString) {
+  if (!inputString || inputString.trim() === '') {
+    return "empty";
+  }
+  const validationRegex = /^(?:(?:id|cluster|metadata\.[\w\.\=\-]+|locality\.?(?:zone|sub_zone|region))=[a-zA-Z0-9_-]+ ?)*$/;
+  return validationRegex.test(inputString);
+}
 
 window.addEventListener('DOMContentLoaded', () => {
   const match = document.cookie.match(/(?:^|; )node_expression=([^;]*)/);
   if (match) {
     input.value = match[1];
+  }
+});
+
+input.addEventListener('input', (event) => {
+  const result = validateInput(event.target.value);
+  if (result === "empty") {
+    input.className = "input is-dark";
+    inputMessage.innerHTML = "";
+  } else if (result === true) {
+    input.className = "input is-success";
+    inputMessage.innerHTML = "";
+  } else {
+    input.className = "input is-danger";
+    inputMessage.innerHTML = "The node filter expression may have no effect, or be invalid";
   }
 });
 
